af5903caf6622fd19af47d09c3b1dff7edbe110a
leeonscoding <leeonscoding@gmail.com>
2023-07-28 00:26:03 +0600

    Zip code

---
+++ src/main/java/org/leeonscoding/zip/MyZipUtil.java
@@ -0,0 +1,36 @@
+package org.leeonscoding.zip;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class MyZipUtil {
+    public void zip(List<String> fileList, String destinationPath) {
+
+        String outputFileName = Long.toString(System.currentTimeMillis())+".zip";
+
+        String destination = destinationPath.concat("/")
+                                            .concat(outputFileName);
+
+        try (
+                ZipOutputStream zipOutputStream = new ZipOutputStream(new FileOutputStream(destination))
+        ) {
+            for (String fileStr : fileList) {
+                FileInputStream fis = new FileInputStream(fileStr);
+                zipOutputStream.putNextEntry(new ZipEntry(fileStr));
+
+                byte[] bytes = new byte[1024];
+                int length;
+                while ((length = fis.read(bytes)) >= 0) {
+                    zipOutputStream.write(bytes, 0, length);
+                }
+                fis.close();
+            }
+        } catch (IOException e) {
+            System.err.println(e.toString());
+        }
+    }
+}
---
+++ src/main/java/org/leeonscoding/zip/TestClient.java
@@ -0,0 +1,12 @@
+package org.leeonscoding.zip;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+public class TestClient {
+    public static void main(String[] args) {
+        MyZipUtil zip = new MyZipUtil();
+        zip.zip(Arrays.asList("test1.txt", "test2.txt"), new File("").getAbsolutePath());
+    }
+}
